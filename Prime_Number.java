01828a0cc0b39222c757abf96c2905567d4779e4
thanhtruong0123 <117093233+thanhtruong0123@users.noreply.github.com>
2023-01-12 18:05:22 +0700

    Add files via upload

---
+++ Prime_Number.java
@@ -0,0 +1,25 @@
+package no2_Loop;
+
+import java.util.Scanner;
+
+public class Prime_Number {
+    static int count = 1;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập số lượng số nguyên tố bạn muốn?");
+        int number =  sc.nextInt();
+
+        for (int i = 0; ; i++) {
+            for (int j = 2; j < i; j++) {
+                if (i % j != 0) {
+                    System.out.print(i + ", ");
+                    count++;
+                    break;
+                }
+            }
+            if (count > number) {
+                break;
+            }
+        }
+    }
+}
